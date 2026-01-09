ecd6c1d3575f5201f74dcef32b6e7c0891c71fd3
Ebuka <davidachibiri8@gmail.com>
2026-01-09 20:22:01 +0200

    Project Rescaffolding due to Flutter dependency incompatibilities

--- android/app/src/main/kotlin/com/ndunari/ndunari/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ndunari.ndunari
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/navigation/app_routes.dart
+++
@@ -1,8 +0,0 @@
-/// Route names for navigation throughout the app
-class AppRoutes {
-  static const String home = '/home';
-  static const String forensicScan = '/forensic-scan';
-  static const String stewardship = '/stewardship';
-  static const String profile = '/profile';
-  static const String history = '/history';
-}
